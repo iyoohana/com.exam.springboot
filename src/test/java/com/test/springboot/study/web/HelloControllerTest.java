5c67171a767e56eb16a58e296b2d582f9e1fa3f9
hnyoo <hnyoo@hist.co.kr>
2021-07-19 14:57:30 +0900

    step.6

--- src/test/java/com/test/springboot/study/web/HelloControllerTest.java
+++ src/test/java/com/test/springboot/study/web/HelloControllerTest.java
@@ -13,6 +13,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/*
+Step 7
+    HelloControllerTest 처럼 맨 뒤에 Test 붙이는 것이 관례
+
+    @ExtendWith : JUnit4 에사 @RunWith
+    스프링 부트 테스트와 JUnit 연결을 수행
+
+    @WebMvcTest
+    @Controller, @ControllerAdvice 등을 사용가능
+    Service, Repository 등에서는 사용 불가
+
+    STATUS
+        cf. Google : HTTP RFC
+ */
+
+
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
